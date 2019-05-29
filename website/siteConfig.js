da29d3964a7ae61e614dc287f949e4e5339e9618
Raytek <raytek.net@gmail.com>
2019-05-29 15:46:19 -0300

    [fix] Fixing baseUrl parameter.

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'Raytek', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://Raytek.github.com', // Your website URL
-  baseUrl: 'Raytek.github.io', // Base URL for your project */
+  baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
